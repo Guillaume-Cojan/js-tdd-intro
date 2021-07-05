ac72950af69942f55b24c733c2dcc898c6f5bebb
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-05 12:40:52 +0300

    last commit

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,17 +2,14 @@ const assert = require("assert");
 
 // WRITE CAPITALIZE FIRST LETTERS
 
-function capitalizeFirstLetters(input) {
-    return input
-        ? [...input]
-              .map((char, index) => {
-                  return index === 0 || input[index - 1] === " "
-                      ? input[index].toUpperCase()
-                      : input[index];
-              })
-              .join("")
-        : input;
-}
+const capitalizeFirstLetters = (str) => {
+    return str.length > 0
+        ? str
+              .split(" ")
+              .map((word) => word[0].toUpperCase() + word.slice(1))
+              .join(" ")
+        : "";
+};
 
 // Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
@@ -20,14 +17,17 @@ assert.strictEqual(typeof capitalizeFirstLetters, "function");
 // Check that capitalizeFirstLetters accepts one argument
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(""), "");
+
+// Check that it works for a 1-letter string
+assert.strictEqual(capitalizeFirstLetters("a"), "A");
+
+// Check that it works for a 1-word string
+assert.strictEqual(capitalizeFirstLetters("russia"), "Russia");
+
 // Check that capitalizeFirstLetters transforms a multiple word phrase
 assert.strictEqual(
     capitalizeFirstLetters("i am learning TDD"),
     "I Am Learning TDD"
 );
-
-// Check that it works for a 1-word string
-assert.strictEqual(capitalizeFirstLetters("russia"), "Russia");
-
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirstLetters(""), "");
