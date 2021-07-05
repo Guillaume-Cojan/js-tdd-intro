7fb4ea3141c3b43da51fdc67f67078317ebb3843
Guillaume-Cojan <guillaume.cojan@gmail.com>
2021-07-05 11:17:55 +0300

    quest-1

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,27 @@
+// capitalizeFirst.js
+const assert = require("assert");
+
+// WRITE THE ACTUAL FUNCTION HERE
+function capitalizeFirst(input) {
+    return input
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirst, "function");
+
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirst.length, 1);
+
+// Check that capitalizeFirst transforms i am learning TDD correctly
+assert.strictEqual(capitalizeFirst("i am learning TDD"), "I Am Learning TDD");
+
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirst("learning"), "Learning");
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(""), "");
+
+capitalizeFirst()
